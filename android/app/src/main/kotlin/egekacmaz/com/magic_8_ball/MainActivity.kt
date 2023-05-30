190d5f03011da15bda7602b719dbc50aa09b1dff
Ege Kacmaz <kcmz.ege@gmail.com>
2023-05-30 17:25:46 -0400

    Create a Stateless Widget

---
+++ android/app/src/main/kotlin/egekacmaz/com/magic_8_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package egekacmaz.com.magic_8_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: const Text('Ask Me Anything'),
+      ),
+      body: Container(),
+    );
+  }
+}
